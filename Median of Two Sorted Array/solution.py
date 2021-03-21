d620057c7736b9ff65f544e1001ba70b2e59ca47
Zachary Chia <xqchia614@outlook.com>
2021-03-21 14:49:19 +0800

    Median of Two Sorted Array: unsolved

---
+++ Median of Two Sorted Array/solution.py
@@ -0,0 +1,10 @@
+import itertools
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        """
+        Join and Median
+        ---------------
+        Time Complexity:    
+        Space Complexity:   
+        """
