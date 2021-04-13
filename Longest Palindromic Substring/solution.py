62f3f274161123087bd6b854ac47f44ff3f5ffdb
Zachary Chia <xqchia614@outlook.com>
2021-04-13 15:26:26 +0800

    Longest Palindromic Substring: Dynamic Programming

--- Longest Palindromic Substring/solution.py
+++ Longest Palindromic Substring/solution.py
@@ -24,8 +24,40 @@ class Solution:
 
     def longestPalindrome2(self, s: str) -> str:
         """
-        Recursive function
+        Dynamic Programming
         ------------------
-        Time Complexity:    
-        Space Complexity:   
+        Time Complexity:    O(n^2)
+        Space Complexity:   O(n^2)
         """
+
+        length = len(s)
+        if length < 2:
+            return s
+
+        sub_max_len = 1
+        begin = 0
+
+        dp_matrix = [[False] * length for _ in range(length)]
+
+        # Each character in the string is itself a palindromic substring of length 1
+        for i in range(length):
+            dp_matrix[i][i] = True
+
+        for sub_length in range(2, length + 1):
+            for start in range(length):
+                end = sub_length + start - 1
+                if end >= length:
+                    break
+
+                if s[start] != s[end]:
+                    dp_matrix[start][end] = False
+                else:
+                    if end - start < 3:
+                        dp_matrix[start][end] = True
+                    else:
+                        dp_matrix[start][end] = dp_matrix[start + 1][end - 1]
+                if dp_matrix[start][end] and end - start + 1 > sub_max_len:
+                    sub_max_len = end - start + 1
+                    begin = start
+
+        return s[begin:begin + sub_max_len]
